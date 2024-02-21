ca699c2d0d3c1a2de836ed1aa891d41007358188
Viktor Sersik <viktor.sersik@gmail.com>
2024-02-21 19:31:30 +0100

    Changed the retrieval of goals to be same like for teamsheets Also fixed scraper (previously, did not scrape events after striked through)

--- app/src/main/java/com/example/waterpoloresults/ui/compose/league/TopScorerList.kt
+++ app/src/main/java/com/example/waterpoloresults/ui/compose/league/TopScorerList.kt
@@ -19,6 +19,7 @@ import com.example.waterpoloresults.ui.theme.WaterpoloResultsTheme
 import commons.Game
 import commons.GameResult
 import commons.League
+import commons.TeamSheet
 import commons.gameevents.GoalGameEvent
 
 @Composable
@@ -28,16 +29,11 @@ fun TopScorerList(
 ) {
     val games by remember { mutableStateOf(leagues.flatMap { it.games }) }
 
-    //TODO: fix matchesPlayed
     val matchesPlayed = games.flatMap { it.result?.teamSheets ?: emptyList() }
         .flatMap { it.players }.map { it.name }
         .groupBy { it }.mapValues { it.value.size }
 
-    //TODO: fix goalScorerList to be gotten from teamsheets instead of gameEvents
-    val goalScorerList = games.flatMap { it.result?.gameEvents ?: emptyList() }
-        .filter { it is GoalGameEvent }.map { it as GoalGameEvent }
-        .groupBy { it.scorerName }
-        .mapValues { it.value.size }
+    val goalScorerList = getGoalScorerList(games)
         .toList().sortedBy{ matchesPlayed[it.first] }.sortedByDescending { it.second }.toMap()
 
     Card(modifier = modifier.padding(8.dp)) {
@@ -64,6 +60,32 @@ fun TopScorerList(
     }
 }
 
+fun getGoalScorerList(games: List<Game>): Map<String, Int> {
+    val result = mutableMapOf<String, Int>()
+
+    for (game in games) {
+        val gameEvents = game.result?.gameEvents ?: emptyList()
+
+        for (i in 0..1) {
+            val team = game.result?.teamSheets?.get(i) ?: continue
+
+            val playerNames = team.players.associate { it.number to it.name }
+            val goals = gameEvents.filter { it is GoalGameEvent && it.scorerTeamHome == (i == 0) }
+                .groupBy { (it as GoalGameEvent).scorerNumber }
+                .mapValues { it.value.size }
+            
+            val scorers = playerNames.map { it.value to (goals[it.key] ?: 0)}.toMap()
+                .filter { it.value > 0 }
+
+            scorers.forEach {
+                result.merge(it.key, it.value) { v1, v2 -> v1 + v2 }
+            }
+        }
+    }
+
+    return result
+}
+
 @Preview(showBackground = true)
 @Preview(showBackground = true, uiMode = Configuration.UI_MODE_NIGHT_YES)
 @Composable
@@ -75,37 +97,39 @@ fun TopScorerListPreview() {
                     Game(
                         result = GameResult(
                             gameEvents = listOf(
-                                GoalGameEvent(scorerName = "Player 1"),
-                                GoalGameEvent(scorerName = "Player 2"),
-                                GoalGameEvent(scorerName = "Player 1"),
-                                GoalGameEvent(scorerName = "Player 2"),
-                                GoalGameEvent(scorerName = "Player 2"),
-                                GoalGameEvent(scorerName = "Player 2"),
-                                GoalGameEvent(scorerName = "Player 3")
+                                GoalGameEvent(scorerName = "Player 1", scorerNumber = 1),
+                                GoalGameEvent(scorerName = "Player 2", scorerNumber = 2),
+                                GoalGameEvent(scorerName = "Player 1", scorerNumber = 1),
+                                GoalGameEvent(scorerName = "Player 2", scorerNumber = 2),
+                                GoalGameEvent(scorerName = "Player 2", scorerNumber = 2),
+                                GoalGameEvent(scorerName = "Player 2", scorerNumber = 2),
+                                GoalGameEvent(scorerName = "Player 3", scorerNumber = 3),
                             ),
                             teamSheets = listOf(
-                                commons.TeamSheet(
+                                TeamSheet(
                                     players = listOf(
-                                        commons.TeamSheet.Player(name = "Player 1"),
-                                        commons.TeamSheet.Player(name = "Player 2"),
-                                        commons.TeamSheet.Player(name = "Player 3")
+                                        TeamSheet.Player(name = "Player 1", number = 1),
+                                        TeamSheet.Player(name = "Player 2", number = 2),
+                                        TeamSheet.Player(name = "Player 3", number = 3)
                                     )
-                                )
+                                ),
+                                TeamSheet()
                             )
                         )
                     ),
                     Game(
                         result = GameResult(
                             gameEvents = listOf(
-                                GoalGameEvent(scorerName = "Player 1"),
-                                GoalGameEvent(scorerName = "Player 1")
+                                GoalGameEvent(scorerName = "Player 1", scorerNumber = 1),
+                                GoalGameEvent(scorerName = "Player 1", scorerNumber = 1),
                             ),
                             teamSheets = listOf(
-                                commons.TeamSheet(
+                                TeamSheet(
                                     players = listOf(
-                                        commons.TeamSheet.Player(name = "Player 1")
+                                        TeamSheet.Player(name = "Player 1", number = 1),
                                     )
-                                )
+                                ),
+                                TeamSheet()
                             )
                         )
                     )
